13a402abac730ea898041cf09386466365a9c708
zimoluo <zimo_luo@outlook.com>
2023-04-01 17:51:51 +0800

    remake

--- main.py
+++ main.py
@@ -1,75 +1,38 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, request, jsonify
 import random
 
 app = Flask(__name__)
-app.secret_key = "setitrandomly" # remember to change this, unless you want others to directly access the data of current deck
 
-suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
-ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+# in-memory database to store game state
+games = {}
 
-def new_deck():
-    deck = [(rank, suit) for rank in ranks for suit in suits]
-    random.shuffle(deck)
-    return deck
+@app.route('/game', methods=['POST'])
+def new_game():
+    # create a new game with a random player order
+    players = request.json['players']
+    random.shuffle(players)
+    games[request.json['game_id']] = {'players': players, 'turn': 0, 'cards': []}
+    return jsonify({'success': True})
 
-rooms = {}
-num_rooms = 0
-room_numbers = {}
+@app.route('/game/<game_id>/play', methods=['POST'])
+def play_card(game_id):
+    # check if game exists
+    if game_id not in games:
+        return jsonify({'success': False, 'message': 'Game not found'})
 
-@app.route("/")
-def index():
-    
-    if "room" not in session:
-        session["room"] = 1
+    # check if it's the player's turn
+    current_player = games[game_id]['players'][games[game_id]['turn']]
+    if current_player != request.json['player']:
+        return jsonify({'success': False, 'message': 'Not your turn'})
 
-    if session["room"] not in rooms:
-        rooms[session["room"]] = [new_deck(), [], []]
+    # add the played card to the discard pile
+    card = request.json['card']
+    games[game_id]['cards'].append(card)
 
-    if "player" not in session:
-        player = 1
-        session["player"] = player
-        rooms[session["room"]][1].append(player)
-        rooms[session["room"]][2].append([rooms[session["room"]][0].pop() for i in range(2)])
-    else:
-        player = session["player"]
+    # advance to the next player's turn
+    games[game_id]['turn'] = (games[game_id]['turn'] + 1) % len(games[game_id]['players'])
 
-    deck, players, hands = rooms[session["room"]]
-    hand = hands[players.index(player)]
-    points = total_points(hand)
+    return jsonify({'success': True})
 
-    if request.args.get("hit"):
-        hand.append(deck.pop())
-        points = total_points(hand)
-        if points > 21:
-            return render_template("lose.html")
-
-    return render_template("start.html", hand=hand, points=points)
-
-@app.route("/room/<room_number>")
-def room(room_number):
-    if "room" not in session:
-        return "Error: Room not found in session"
-
-    if "player" not in session:
-        return "Error: Player not found in session"
-
-    player = session["player"]
-    room = session["room"]
-    deck, players, hands = rooms[room]
-    my_hand = hands[players.index(player)]
-    my_points = total_points(my_hand)
-
-    dealer_hand = hands[-1]
-    while total_points(dealer_hand) < 17:
-        dealer_hand.append(deck.pop())
-
-    dealer_points = total_points(dealer_hand)
-
-    if my_points > 21:
-        return render_template("lose.html")
-    elif dealer_points > 21:
-        return render_template("win.html")
-    elif my_points > dealer_points:
-        return render_template("win.html")
-    else:
-        return render_template("lose.html")
+if __name__ == '__main__':
+    app.run(debug=True)
